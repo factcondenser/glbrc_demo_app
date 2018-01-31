dd302a56212ff30d2383ad80bf334570b9ae70c0
factcondenser <markpan@live.com>
2018-01-30 22:19:18 -0600

    Migrate to postgresql and update seeds.rb

--- Gemfile
+++ Gemfile
@@ -15,12 +15,11 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bcrypt', '~> 3.1.7'
+gem 'pg', '0.21.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', '9.0.6'
-  # Use sqlite3 as the database for Active Record
-  gem 'sqlite3', '1.3.13'
 end
 
 group :development do
@@ -37,10 +36,6 @@ group :test do
   gem 'minitest-reporters',       '1.1.14'
   gem 'guard',                    '2.14.1'
   gem 'guard-minitest',           '2.4.6'
-end
-
-group :production do
-  gem 'pg', '0.21.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{ login: 'user1', password: 'glbrcpass'}, { login: 'user2', password: 'glbrcpass'}, { login: 'user3', password: 'glbrcpass'}])
+Application.create([
+  { name: 'Google', description: 'Search Engine', color: 'Red', default_status: 'Yes', link: 'https://www.google.com/' },
+  { name: 'Wisc', description: 'UW homepage', color: 'Blue', default_status: 'No', link: 'https://www.wisc.edu/' },
+  { name: 'GLBRC', description: 'Great Lakes Bioenergy Research Center', color: 'Yellow', default_status: 'Yes', link: 'https://www.glbrc.org/' },
+  { name: 'WEI', description: 'Wisconsin Energy Institute', color: 'Green', default_status: 'No', link: 'https://energy.wisc.edu/' },
+  { name: 'Twitter', description: 'Twitter', color: 'Purple', default_status: 'No', link: 'https://twitter.com/' }
+])
